508ff08fe898c229a905b4143503836f9f5a14d1
Connor Lindsey <cblindsey3@gmail.com>
2024-08-03 18:57:55 -0700

    Exercise 21

---
+++ exercises/21-models/script.js
@@ -0,0 +1,183 @@
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/Addons.js"
+import GUI from "lil-gui"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
+
+/**
+ * Setup
+ */
+// Debug
+const gui = new GUI({
+  width: 300,
+  title: "Debug",
+  closeFolders: true,
+})
+const dbg = {}
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "h") {
+    gui.show(gui._hidden)
+  }
+})
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
+// Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x000000)
+
+// Camera
+let sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(2, 2, 2)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+// Handle resizing
+window.addEventListener("resize", () => {
+  sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  // Adjust pixel ratio for higher res displays, cap at 2x
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Double click to toggle fullscreen
+window.addEventListener("dblclick", () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+})
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  }),
+)
+floor.receiveShadow = true
+floor.rotation.x = -Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = -7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = -7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath("/draco/")
+gltfLoader.setDRACOLoader(dracoLoader)
+
+// gltfLoader.load("/models/Duck/glTF-Draco/Duck.gltf", (gltf) => {
+//   console.log(gltf)
+//   // Just add duck object to the scene
+//   scene.add(gltf.scene.children[0])
+// })
+
+let mixer = null
+
+gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
+  gltf.scene.scale.set(0.02, 0.02, 0.02)
+  scene.add(gltf.scene)
+
+  mixer = new THREE.AnimationMixer(gltf.scene)
+  const action = mixer.clipAction(gltf.animations[0])
+  action.play()
+})
+
+// gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+//   // Copy children to avoid moving array indexes
+//   // Or use while loop and add first child
+//   const children = [...gltf.scene.children]
+//   for (const child of children) {
+//     scene.add(child)
+//   }
+// })
+
+/**
+ * Render
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
+
+  // Update animations
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
+
+  // Update controls
+  controls.update()
+
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
+}
+tick()
